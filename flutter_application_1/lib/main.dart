f8564a824ed7419d83701e30d37f34d44cbce164
Hassan-9526 <ha4383731@gmail.com>
2023-11-25 23:02:06 +0500

    day4 complete

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/Home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,11 +13,16 @@ class MyApp extends StatelessWidget {
     bringvegtables(thaila: true);
     return MaterialApp(
       // home: HomePage(),
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.teal),
+
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        fontFamily: GoogleFonts.balooBhai2().fontFamily,
+      ),
+
       darkTheme:
           ThemeData(brightness: Brightness.dark, primarySwatch: Colors.grey),
-      initialRoute: "/home",
+      // initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
--- flutter_application_1/lib/pages/login_page.dart
+++ flutter_application_1/lib/pages/login_page.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login page",
-            style: TextStyle(
-                fontSize: 40,
-                color: Colors.blueGrey,
-                backgroundColor: Colors.white60,
-                fontWeight: FontWeight.w900,
-                letterSpacing: 3)),
+        // borderRadius: BorderRadius.circular(2300),
+        // ignore: prefer_const_constructors
+        child: Column(children: [
+      Image.asset("assets/images/login_image.png", fit: BoxFit.contain),
+      const SizedBox(
+        height: 5.0,
       ),
-    );
+      const Text(
+        "Welcome to login page",
+        style: TextStyle(
+          color: Colors.blueGrey,
+          fontSize: 50,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      const SizedBox(
+        height: 20.0,
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 36.0),
+        child: Column(children: [
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: "Enter username", labelText: "Username"),
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: "Enter Password", labelText: "Password"),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          ElevatedButton(
+            child: Text("Login"),
+            // style: TextButton.styleFrom(foregroundColor: Colors.cyan),
+            onPressed: () => {print("hello hassan")},
+          )
+        ]),
+      ),
+    ]));
   }
 }
